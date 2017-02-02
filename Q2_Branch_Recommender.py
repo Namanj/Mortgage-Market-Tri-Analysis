a746737f411bed96eb92c1ff895fdfca8cb89fa2
namanj <naman.jain020@gmail.com>
2017-02-01 19:42:49 -0800

    Almost done with Q2

---
+++ Q2_Branch_Recommender.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import random
+import cPickle as pickle
+
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import seaborn as sns
+from shapely.geometry import Point
+
+from scipy.optimize import basinhopping
+
+random.seed(0)
+
+class RandomDisplacementPolygonCheck(object):
+    def __init__(self, state_polygon, state_shapefile, stepsize = 10):
+        self.stepsize = stepsize
+        self._state_polygon = state_polygon
+        self._state_shapefile = state_shapefile
+
+    def __call__(self, x):
+        while True:
+            xnew = x + np.random.uniform(-self.stepsize, self.stepsize, np.shape(x))
+            
+            # Checking whether proposed point is within the Polygons boundry
+            if filter(self._state_polygon.contains, [Point(self._state_shapefile(xnew[0], xnew[1]))]):
+                break          
+        return xnew
+    
+
+class CallbackCondition(object):
+    def __init__(self):
+        self.list_of_new_points = []
+        self.num_of_points = 0
+
+    def __call__(self, x_new, f, accept):
+        if accept:
+            self.list_of_new_points.append((x_new, f))
+    
+class BranchRecommender(object):
+	def __init__(self, df, state_polygon, _state_shapefile):
+		self._df = df
+		self._state_polygon = state_polygon
+		self._state_shapefile = _state_shapefile
+		pass
+
+	def initiate(self):
+		# an example function with multiple minima
+		def f(x): return np.sin(np.linalg.norm(x) * np.pi)
+
+		# the starting point, starting from roughly the middle of the polygon
+		x0 = [self._df['Long'].mean(), self._df['Lat'].mean()]
+
+		# the bounds
+		xmin = [self._df['Long'].min(), self._df['Lat'].min()]
+		xmax = [self._df['Long'].max(), self._df['Lat'].max()]
+
+		# rewrite the bounds in the way required by L-BFGS-B
+		bounds = [(low, high) for low, high in zip(xmin, xmax)]
+		minimizer_kwargs = dict(method = "L-BFGS-B", bounds = bounds)
+
+		# define the new step taking routine and pass it to basinhopping
+		take_step = RandomDisplacementPolygonCheck(self._state_polygon, self._state_shapefile)
+		callback = CallbackCondition()
+
+		list_of_new_points = []
+		for i in range(10):
+		    result = basinhopping(f, x0, niter = 10, minimizer_kwargs = minimizer_kwargs, interval = 100,
+		                      take_step = take_step, callback = callback)
+		    list_of_new_points.append(result)
+		
+		# print callback.list_of_new_points
+		# print list_of_new_points
+
+
+if __name__ == '__main__':
+	br = BranchRecommender()
+	br.initiate()
+
---
+++ Q2_State_Plotter.py
@@ -0,0 +1,170 @@
+import pandas as pd
+import numpy as np
+import random
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.collections import PatchCollection
+from mpl_toolkits.basemap import Basemap
+from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
+from shapely.prepared import prep
+import seaborn as sns
+
+from lxml import etree
+from descartes import PolygonPatch
+import fiona
+from itertools import chain
+from pygeocoder import Geocoder
+
+import shapefile as shp
+
+from Q2_Branch_Recommender import BranchRecommender
+
+random.seed(0)
+
+
+class initiate(object):
+	"""docstring for initiate"""
+	def __init__(self, state, num):
+		self._state = state
+		self._num = num
+		self._cities = None
+		self._df_cities_lat_long = None
+		self._state_shapefile = None
+		self._df_map = None
+		self._state_points = None
+		self.state_polygon = None
+
+	def get_data(self):
+		df = pd.read_excel('1.3 Portfolio data for last three years full.xlsx', sheetname = 'Branch', skiprows = 3, \
+										names = ['S.No','City', 'State', 'Branch Opening Date'])
+
+		df.drop('S.No', axis = 1, inplace = True)
+		df_by_state = df.groupby(['State', 'City', 'Branch Opening Date']).count()
+
+		self._cities = df_by_state.ix[self._state].reset_index()['City']
+		self._df_cities_lat_long = pd.DataFrame(columns = ['City', 'Lat', 'Long'])
+
+		for i in range(len(self._cities)):
+			# Getting Lat & Long for each city in self._state and appending it to self._df_cities_lat_long
+		    (latitude, longitude) = Geocoder.geocode(self._cities[i] + self._state).coordinates
+		    self._df_cities_lat_long.loc[i] = [self._cities[i], latitude, longitude]
+
+		pass		
+
+	def read_shapefile(self):
+		self._num = ['0{}'.format(self._num) if self._num < 10 else self._num][0]
+		path = "gov data/maps/eci/PC_Data/States/S{num}/S{num}_PC.shp".format(num = self._num)
+		shp = fiona.open(path)
+		bds = shp.bounds
+		shp.close()
+
+		extra = 0.01
+		ll = (bds[0], bds[1])
+		ur = (bds[2], bds[3])
+		self._coords = list(chain(ll, ur))
+		w, h = self._coords[2] - self._coords[0], self._coords[3] - self._coords[1]
+		
+		self._state_shapefile = Basemap(
+			lon_0 = self._df_cities_lat_long['Long'].mean(), 
+			lat_0 = self._df_cities_lat_long['Lat'].mean(), 
+			projection = 'tmerc', ellps = 'WGS84',
+		    llcrnrlon = self._coords[0] - extra * w,					# llcrnrlon = Lower Left Corner Longitude
+		    llcrnrlat = self._coords[1] - extra + 0.01 * h,				# llcrnrlat = Lower Left Corner Latitude
+		    urcrnrlon = self._coords[2] + extra * w,					# urcrnrlon = Upper Right Corner Longitude
+		    urcrnrlat = self._coords[3] + extra + 0.01 * h,				# urcrnrlat = Upper Right Corner Latitude
+		    lat_ts = 0, resolution = 'i', suppress_ticks = True)
+
+		self._state_shapefile.readshapefile(
+		    'gov data/maps/eci/PC_Data/States/S{num}/S{num}_PC'.format(num = self._num),
+		    self._state,
+		    color = 'none', zorder = 2)
+
+		pass
+
+	def map_df(self):
+	    # set up a map dataframe
+		a = 'state_info'.format(state = self._state)
+		self._df_map = pd.DataFrame(
+							{
+						    'poly': [Polygon(xy) for xy in self._state_shapefile.UP],
+						    'PC_NAME': [district['PC_NAME'] for district in self._state_shapefile.UP_info]
+						    })
+
+		self._df_map['area_m'] = self._df_map['poly'].map(lambda item: item.area)
+		self._df_map['area_km'] = self._df_map['area_m'] / 100000
+
+		# Create Point objects in map coordinates from dataframe lon and lat values
+		map_points = pd.Series([Point(self._state_shapefile(mapped_x, mapped_y)) for mapped_x, mapped_y in \
+						zip(self._df_cities_lat_long['Long'], self._df_cities_lat_long['Lat'])])
+		self._state_points = MultiPoint(list(map_points.values))
+		self.state_polygon = prep(MultiPolygon(list(self._df_map['poly'].values)))
+
+		# calculate points that fall within the state boundary
+		self._state_points = filter(self.state_polygon.contains, self._state_points)
+
+		# draw state districts from polygons
+		self._df_map['districts'] = self._df_map['poly'].map(lambda item: PolygonPatch(
+		    item,
+		    fc='#555555',
+		    ec='#787878', lw=.25, alpha=.9,
+		    zorder=4))
+
+		pass
+
+	def plot_formatted_shapefile_with_locations(self):
+		plt.clf()
+		fig = plt.figure()
+		ax = fig.add_subplot(111, axisbg = 'w', frame_on = False)
+
+		# we don't need to pass points to m() because we calculated using map_points and shapefile polygons
+		dev = self._state_shapefile.scatter(
+		    [geom.x for geom in self._state_points],
+		    [geom.y for geom in self._state_points],
+		    20, marker = 'o', lw = .25, facecolor = '#33ccff', edgecolor = 'w',
+		    alpha = 0.9, antialiased = True, label = 'Blue Shubham office locations', zorder = 3)
+		
+		# plot office locations by adding the PatchCollection to the axes instance
+		ax.add_collection(PatchCollection(self._df_map['districts'].values, match_original = True))
+		
+		# copyright and source data info
+		smallprint = ax.text(
+		    1.03, 0.1,
+		    '\nContains Shubham Housing Finance Proprietary Data',
+		    ha = 'right', va = 'bottom',
+		    size = 8, color = '#555555',
+		    transform = ax.transAxes)
+
+		# Draw a map scale
+		self._state_shapefile.drawmapscale(
+		    self._coords[0] + 0.08, self._coords[1] + 0.015,
+		    self._coords[0], self._coords[1], 10.,
+		    barstyle = 'fancy', labelstyle = 'simple',
+		    fillcolor1 = 'w', fillcolor2 = '#555555',
+		    fontcolor = '#555555', zorder = 5)
+
+		plt.title("Shubham Housing Finance Office Locations, {State}".format(State = self._state))
+		plt.tight_layout()
+
+		# this will set the image width to 722px at 100dpi
+		fig.set_size_inches(10, 10)
+
+		# plt.savefig('data/{State}_visualized.png'.format(State = self._state), dpi = 100, alpha = True)
+		plt.show()
+			
+		pass	
+
+
+if __name__ == '__main__':
+	state = 'UP'
+	num = 24
+	
+	start = initiate(state, num)
+	start.get_data()
+	start.read_shapefile()
+	start.map_df()
+	start.plot_formatted_shapefile_with_locations()
+
+	br = BranchRecommender(start._df_cities_lat_long, start.state_polygon, start._state_shapefile)
+	br.initiate()
+
